f131dd63b63704b032b943a468f5eb303aeffad3
SkepTic <antonlpf@icloud.com>
2024-01-03 23:20:36 +0400

    adding outlet, changing tableview background

--- ImageFeed/ImagesListViewController.swift
+++ ImageFeed/ImagesListViewController.swift
@@ -8,7 +8,9 @@
 import UIKit
 
 class ImagesListViewController: UIViewController {
-
+    
+    @IBOutlet private var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
