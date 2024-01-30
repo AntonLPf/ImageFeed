d9eb05cacb6732c4528e7f5072d424789be44589
SkepTic <antonlpf@icloud.com>
2024-01-30 19:39:48 +0400

    refactoring profileView eith manual layout

---
+++ ImageFeed/ProjectConstants.swift
@@ -0,0 +1,27 @@
+//
+//  ProjectColor.swift
+//  ImageFeed
+//
+//  Created by Антон Шишкин on 30.01.24.
+//
+
+import Foundation
+
+struct ProjectConstants {
+    
+    enum Color: String {
+        case ypBlack = "YPBlack"
+        case ypWhite = "YPWhite"
+        case ypGray = "YPGray"
+    }
+    
+    enum Font: String {
+        case ysDisplayBold = "YSDisplay-Bold"
+        case ysDisplayMedium = "YSDisplay-Medium"
+    }
+    
+    enum Picture: String {
+        case exitButton = "ExitButton"
+        case profilePicture = "ProfilePicture"
+    }
+}
