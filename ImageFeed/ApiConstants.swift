e5d5cdfc1fe2d11f2b9792af169f27ac57f83649
SkepTic <antonlpf@icloud.com>
2024-02-05 12:29:29 +0400

    adding apiconstants

---
+++ ImageFeed/ApiConstants.swift
@@ -0,0 +1,16 @@
+//
+//  ApiConstants.swift
+//  ImageFeed
+//
+//  Created by Антон Шишкин on 05.02.24.
+//
+
+import Foundation
+
+struct ApiConstants {
+    static let accesKey = "gOcCwhsMrjRXy83ALiImrfQhnm2-NoA5TL3-MeHbRMA"
+    static let secretKey = "EI7spYRB6F_WOjKYB2r2uB3Npg9ctzv17v7AV_IgD9Y"
+    static let redirectUri = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseUrl = URL(string: "https://api.unsplash.com")
+}
