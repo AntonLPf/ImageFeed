17ba0cc9d61093b6dcebd8b728a0c26e233e9291
SkepTic <antonlpf@icloud.com>
2024-03-20 16:07:31 +0400

    adding webView tests

--- ImageFeedTests/WebViewTests.swift
+++ ImageFeedTests/WebViewTests.swift
@@ -8,26 +8,45 @@
 @testable import ImageFeed
 import XCTest
 
-final class WebViewPresenterSpy: WebViewPresenterProtocol {
-    var viewDidLoadCalled: Bool = false
-    var view: WebViewViewControllerProtocol?
-    
-    func viewDidLoad() {
-        viewDidLoadCalled = true
-    }
-    
-    func didUpdateProgressValue(_ newValue: Double) {
-        
-    }
-    
-    func code(from url: URL) -> String? {
-        nil
-    }
-}
-
 final class WebViewTests: XCTestCase {
     
+    final class WebViewPresenterSpy: WebViewPresenterProtocol {
+        var viewDidLoadCalled: Bool = false
+        var view: WebViewViewControllerProtocol?
+        
+        func viewDidLoad() {
+            viewDidLoadCalled = true
+        }
+        
+        func didUpdateProgressValue(_ newValue: Double) {
+            
+        }
+        
+        func code(from url: URL) -> String? {
+            nil
+        }
+    }
+    
+    final class WebViewViewControllerSpy: WebViewViewControllerProtocol {
+        var presenter: WebViewPresenterProtocol?
+
+        var loadRequestCalled: Bool = false
+
+        func load(request: URLRequest) {
+            loadRequestCalled = true
+        }
+
+        func setProgressValue(_ newValue: Float) {
+
+        }
+
+        func setProgressHidden(_ isHidden: Bool) {
+
+        }
+    }
+    
     func testViewControllerCallsViewDidLoad() {
+        
         //given
         let vc = WebViewViewController()
         let presenter = WebViewPresenterSpy()
@@ -41,21 +60,80 @@ final class WebViewTests: XCTestCase {
         XCTAssertTrue(presenter.viewDidLoadCalled)
     }
     
-//    func testFetchPhotos() {
-//        let service = ImagesListService.shared
-//        
-//        let expectation = self.expectation(description: "Wait for Notification")
-//        
-//        NotificationCenter.default.addObserver(
-//            forName: ImagesListService.didChangeNotification,
-//            object: nil,
-//            queue: .main) { _ in
-//                expectation.fulfill()
-//            }
-//        
-//        service.fetchPhotosNextPage(<#T##token: String##String#>, <#T##completion: (Result<[Photo], Error>) -> Void##(Result<[Photo], Error>) -> Void#>)
-//        wait(for: [expectation], timeout: 10)
-//        
-//        XCTAssertEqual(service.photos.count, 10)
-//    }
+    func testPresenterCallsLoadRequest() {
+        
+        //given
+        let vc = WebViewViewControllerSpy()
+        let authHelper = AuthHelper()
+        let presenter = WebViewPresenter(authHelper: authHelper)
+        vc.presenter = presenter
+        presenter.view = vc
+        
+        //when
+        presenter.viewDidLoad()
+        
+        //then
+        XCTAssertTrue(vc.loadRequestCalled)
+    }
+    
+    func testProgressVisibleWhenLessThenOne() {
+        //given
+        let authHelper = AuthHelper()
+        let presenter = WebViewPresenter(authHelper: authHelper)
+        let progress: Float = 0.6
+        
+        //when
+        let shouldHideProgress = presenter.shouldHideProgress(for: progress)
+        
+        //then
+        XCTAssertFalse(shouldHideProgress)
+    }
+    
+    func testProgressHiddenWhenOne() {
+        
+        //given
+        let authHelper = AuthHelper() //Dummy
+        let presenter = WebViewPresenter(authHelper: authHelper)
+        let progress: Float = 1.0
+        
+        //when
+        let shouldHideProgress = presenter.shouldHideProgress(for: progress)
+        
+        //then
+        XCTAssertTrue(shouldHideProgress)
+    }
+    
+    func testAuthHelperAuthURL() {
+        
+        //given
+        let configuration = Constants.AuthConfiguration.standard
+        let authHelper = AuthHelper(configuration: configuration)
+        
+        //when
+        let urlRequest = authHelper.authRequest()!
+        let urlString = urlRequest.url!.absoluteString
+        
+        //then
+        XCTAssertTrue(urlString.contains(configuration.authURLString))
+        XCTAssertTrue(urlString.contains(configuration.accessKey))
+        XCTAssertTrue(urlString.contains(configuration.redirectURI))
+        XCTAssertTrue(urlString.contains("code"))
+        XCTAssertTrue(urlString.contains(configuration.accessScope))
+    }
+    
+    func testCodeFromURL() {
+        
+        //given
+        var urlComponents = URLComponents(string: "https://unsplash.com/oauth/authorize/native")!
+        urlComponents.queryItems = [URLQueryItem(name: "code", value: "test code")]
+        let url = urlComponents.url!
+        let authHelper = AuthHelper()
+        
+        //when
+        let code = authHelper.code(from: url)
+        
+        //then
+        XCTAssertEqual(code, "test code")
+    }
+
 }
