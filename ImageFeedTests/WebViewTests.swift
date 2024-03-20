084291a19b65eaca51ebcdb24a6a89db0113a5f7
SkepTic <antonlpf@icloud.com>
2024-03-20 13:42:10 +0400

    adding test testViewControllerCallsViewDidLoad

--- ImageFeedTests/WebViewTests.swift
+++ ImageFeedTests/WebViewTests.swift
@@ -8,23 +8,54 @@
 @testable import ImageFeed
 import XCTest
 
+final class WebViewPresenterSpy: WebViewPresenterProtocol {
+    var viewDidLoadCalled: Bool = false
+    var view: WebViewViewControllerProtocol?
+    
+    func viewDidLoad() {
+        viewDidLoadCalled = true
+    }
+    
+    func didUpdateProgressValue(_ newValue: Double) {
+        
+    }
+    
+    func code(from url: URL) -> String? {
+        nil
+    }
+}
+
 final class WebViewTests: XCTestCase {
     
-    func testFetchPhotos() {
-        let service = ImagesListService.shared
+    func testViewControllerCallsViewDidLoad() {
+        //given
+        let vc = WebViewViewController()
+        let presenter = WebViewPresenterSpy()
+        vc.presenter = presenter
+        presenter.view = vc
         
-        let expectation = self.expectation(description: "Wait for Notification")
+        //when
+        _ = vc.view
         
-        NotificationCenter.default.addObserver(
-            forName: ImagesListService.didChangeNotification,
-            object: nil,
-            queue: .main) { _ in
-                expectation.fulfill()
-            }
-        
-        service.fetchPhotosNextPage(<#T##token: String##String#>, <#T##completion: (Result<[Photo], Error>) -> Void##(Result<[Photo], Error>) -> Void#>)
-        wait(for: [expectation], timeout: 10)
-        
-        XCTAssertEqual(service.photos.count, 10)
+        //then
+        XCTAssertTrue(presenter.viewDidLoadCalled)
     }
+    
+//    func testFetchPhotos() {
+//        let service = ImagesListService.shared
+//        
+//        let expectation = self.expectation(description: "Wait for Notification")
+//        
+//        NotificationCenter.default.addObserver(
+//            forName: ImagesListService.didChangeNotification,
+//            object: nil,
+//            queue: .main) { _ in
+//                expectation.fulfill()
+//            }
+//        
+//        service.fetchPhotosNextPage(<#T##token: String##String#>, <#T##completion: (Result<[Photo], Error>) -> Void##(Result<[Photo], Error>) -> Void#>)
+//        wait(for: [expectation], timeout: 10)
+//        
+//        XCTAssertEqual(service.photos.count, 10)
+//    }
 }
